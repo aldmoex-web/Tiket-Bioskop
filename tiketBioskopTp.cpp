5673e3a6a75ccb2f0f01fba4f59ff8bff5d4e48c
fa/ul <jhondefan2008@gmail.com>
2026-07-09 17:57:04 +0700

    Update tiketBioskopTp.cpp

--- tiketBioskopTp.cpp
+++ tiketBioskopTp.cpp
@@ -6,15 +6,7 @@
 #include <cctype>
 #include <climits>
 
-using namespace std;
 
-template<typename T>
-string to_string(T value)
-{
-    stringstream ss;
-    ss << value;
-    return ss.str();
-}
 
 
    ///CLASS FILM
